463378157b802e1ac7442acef9e4d9ed93107331
Amitava <amitavaghosh1981@gmail.com>
2021-11-23 22:32:11 +0530

    added kafka starter

---
+++ knowledge/startKafkaBroker1.bat
@@ -0,0 +1,4 @@
+cd C:\Code\kafka_2.12-2.2.0\bin\windows
+kafka-server-start.bat C:\Code\kafka_2.12-2.2.0\config\server1.properties
+kafka-server-start.bat C:\Code\kafka_2.12-2.2.0\config\server2.properties
+kafka-server-start.bat C:\Code\kafka_2.12-2.2.0\config\server3.properties
---
+++ knowledge/startZookeeper.bat
@@ -0,0 +1,2 @@
+cd C:\Code\kafka_2.12-2.2.0\bin\windows
+zookeeper-server-start.bat C:\Code\kafka_2.12-2.2.0\config\zookeeper.properties
