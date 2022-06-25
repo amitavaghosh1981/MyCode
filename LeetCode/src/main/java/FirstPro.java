6d1b0d60602d4be12951515a9e2302b421cf407c
Amitava <amitavaghosh1981@gmail.com>
2022-06-25 16:38:25 +0530

    LeetCode commit

--- LeetCode/src/main/java/FirstPro.java
+++ LeetCode/src/main/java/FirstPro.java
@@ -1,5 +1,7 @@
 //We will go with Prakash shkula
 //Two Sum https://www.youtube.com/watch?v=TXxwt1eFF98
+//submission will be maintained in leetcode and push will be done once from here
+//algorithm is sorted
 public class FirstPro {
     public static void main(String[] args){
         System.out.println("a");
