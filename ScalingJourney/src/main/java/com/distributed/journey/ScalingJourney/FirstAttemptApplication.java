ba36b617a1d2a23a39081360e4c732cbbc35be26
Amitava <amitavaghosh1981@gmail.com>
2022-06-30 15:48:18 +0530

    Scaling over kubernates

---
+++ ScalingJourney/src/main/java/com/distributed/journey/ScalingJourney/FirstAttemptApplication.java
@@ -0,0 +1,14 @@
+package com.distributed.journey.ScalingJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAttemptApplication {
+
+	public static void main(String[] args) {
+		System.out.println("FirstAttemptApplication running");
+		SpringApplication.run(FirstAttemptApplication.class, args);
+	}
+
+}
