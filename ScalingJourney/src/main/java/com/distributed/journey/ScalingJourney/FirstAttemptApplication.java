de3dea2e315e581211c39cfe5d7619536fa4750b
Amitava <amitavaghosh1981@gmail.com>
2022-06-30 17:53:10 +0530

    Scaling over kubernates

--- ScalingJourney/src/main/java/com/distributed/journey/ScalingJourney/FirstAttemptApplication.java
+++ ScalingJourney/src/main/java/com/distributed/journey/ScalingJourney/FirstAttemptApplication.java
@@ -2,13 +2,20 @@ package com.distributed.journey.ScalingJourney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstAttemptApplication {
 
-	public static void main(String[] args) {
-		System.out.println("FirstAttemptApplication running");
-		SpringApplication.run(FirstAttemptApplication.class, args);
-	}
+    @RequestMapping("/try")
+    public String home() {
+        return "Hello Docker World";
+    }
 
+    public static void main(String[] args) {
+        System.out.println("FirstAttemptApplication running");
+        SpringApplication.run(FirstAttemptApplication.class, args);
+    }
 }
